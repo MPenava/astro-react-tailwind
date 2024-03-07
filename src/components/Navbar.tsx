a5bab3a5852ab63b1f81577b9f0ce5debc6c9b3e
MPenava <mpenava@royanis.com>
2024-03-07 22:48:14 +0100

    chore: change title

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div className="text-5xl text-gray-500">Navbar</div>;
+  return <div className="text-5xl text-gray-500">Navbar Title</div>;
 };
 
 export default Navbar;
