44c7b4afafcb42934575ebd1afd24c8c319a5e3d
MPenava <mpenava@royanis.com>
2024-03-07 22:57:41 +0100

    chore: change titles of pagesđ

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div className="text-5xl text-gray-500">Astro</div>;
+  return (
+    <div className="text-5xl text-gray-500">
+      Astro + React + Tailwind Project
+    </div>
+  );
 };
 
 export default Navbar;
